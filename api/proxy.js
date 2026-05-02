a232136e1b31e372eb4e7c026ba4b440bebc3971
jryoung201-code <jryoung201@gmail.com>
2026-05-02 00:17:26 -0400

    Create proxy.js
    Fixing vercel loading

---
+++ api/proxy.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).send("Missing URL");
+  }
+
+  try {
+    const response = await fetch(url);
+    const text = await response.text();
+    res.status(200).send(text);
+  } catch (err) {
+    res.status(500).send("Proxy error");
+  }
+}
